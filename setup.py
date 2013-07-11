2a9a389c6e3425a893a049649d29ca35f8527006
Gabriel <g2p.code@gmail.com>
2013-07-12 01:16:12 +0200

    Release v0.3.0.  Changes since the previous release:
    * Better documentation and command-line help
    * Unprivileged networking (KVM only)
    * Privilege transitions for block device access (KVM only)
    * Alternative implementation based on user namespaces
    * Pass command exit status back to vido
    * Export $VIDO_DISK variables for mapped disks
    * Fix the KVM implementation to be text-only

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='vido',
-    version='0.2.0',
+    version='0.3.0',
     author='Gabriel de Perthuis',
     author_email='g2p.code+vido@gmail.com',
     url='https://github.com/g2p/vido',
